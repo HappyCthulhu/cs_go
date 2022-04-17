cd2d8f18c3df86ad078a10eb02cda99df1a6fc16
HappyCthulhu <valeriyg98@gmail.com>
2022-04-17 13:48:58 +0300

    Change win32gui to PIL, add func for find button
    This func can find coords of button, using color of it. Also, func can find center of button. In previous version, button searching was based on center of screen and changing of color of central pixel

--- game_waiting.py
+++ game_waiting.py
@@ -1,51 +1,105 @@
 import os
 import time
 import pyautogui
+
 import wx
+from PIL import ImageGrab
 from notifiers import get_notifier
 
-# TODO: через 30 секунд прислать мне скрин экрана
-# TODO: сделать так, чтоб, если катка не состоялась, он продолжал отслеживать экран
 
-def get_pixel_colour(i_x, i_y):
-    import win32gui
-    i_desktop_window_id = win32gui.GetDesktopWindow()
-    i_desktop_window_dc = win32gui.GetWindowDC(i_desktop_window_id)
-    long_colour = win32gui.GetPixel(i_desktop_window_dc, i_x, i_y)
-    i_colour = int(long_colour)
-    return (i_colour & 0xff), ((i_colour >> 8) & 0xff), ((i_colour >> 16) & 0xff)
+def switch_from_cs_window():
+    pyautogui.keyDown('alt')
+    time.sleep(.2)
+    pyautogui.press('tab')
+    time.sleep(.2)
+    pyautogui.keyUp('alt')
 
 
-def check_presense_of_green_button():
-    time.sleep(1)
+def get_button_coordinates(x_scale, y_scale):
+    coordinates_colours = []
+    all_colours = []
+    coords = []
 
-    if start_colour != get_pixel_colour(half_width, half_height):
-        print('Button present!')
-        return True
-    else:
-        print('Still waiting...')
-        return False
+    image = ImageGrab.grab()
 
+    for x in range(x_scale):
+
+        for y in range(y_scale):
+            coords.append((x, y))
+            colour = image.getpixel((x, y))
+            all_colours.append(colour)
+
+            if colour == button_colour:
+                coordinates_colours.append((x, y))
+
+    return coordinates_colours
+
+
+def get_center_of_the_button(button_coordinates: list):
+    left_border = sorted(button_coordinates, key=lambda coord_colour: coord_colour[0])[0]
+    right_border = sorted(button_coordinates, key=lambda coord_colour: coord_colour[0], reverse=True)[0]
+    upper_border = sorted(button_coordinates, key=lambda coord_colour: coord_colour[1])[0]
+    bottom_border = sorted(button_coordinates, key=lambda coord_colour: coord_colour[1], reverse=True)[0]
+    button_center_horizontal = int(left_border[0] + (right_border[0] - left_border[0]) / 2)
+    button_center_vertical = int(upper_border[1] + (bottom_border[1] - upper_border[1]) / 2)
+    button_center = (button_center_horizontal, button_center_vertical)
+
+    # pyautogui.moveTo(*left_border)
+    # time.sleep(0.5)
+    # pyautogui.moveTo(*right_border)
+    # time.sleep(0.5)
+    # pyautogui.moveTo(*upper_border)
+    # time.sleep(0.5)
+    # pyautogui.moveTo(*bottom_border)
+    # time.sleep(0.5)
+    # pyautogui.moveTo(*button_center)
+    # time.sleep(0.5)
+    #
+    # print(f'left_border: {left_border}\n'
+    #       f'right_border: {right_border}\n'
+    #       f'upper_border: {upper_border}\n'
+    #       f'bottom_border: {bottom_border}\n'
+    #       f'center: {button_center}')
+
+    return button_center
+
+
+def check_existing_of_pixels_with_button_colour(x_scale, y_scale):
+    image = ImageGrab.grab()
+    for x in range(x_scale):
+        colours = []
+
+        for y in range(y_scale):
+
+            colour = image.getpixel((x, y))
+            colours.append(colour)
+
+            if colour != button_colour:
+                pass
+            else:
+                print('Found button colour!')
+                return True
+
+    return False
+
+
+button_colour = (76, 175, 80)
 
 app = wx.App(False)
 width, height = wx.GetDisplaySize()
-
 telegram = get_notifier('telegram')
 
-half_width = int(width / 2)
-half_height = int(height / 2)
+# switch_from_cs_window()
 
-time.sleep(3)
+time.sleep(2)
 
-start_colour = get_pixel_colour(half_width, half_height)
-print(f'Start colour is: {start_colour}')
-
-while not check_presense_of_green_button():
+while not check_existing_of_pixels_with_button_colour(width, height):
     pass
 
-print(f'Button colour is: {get_pixel_colour(half_width, half_height)}')
-pyautogui.click(half_width, half_height, interval=0.5, clicks=5)
-print('Clicked the button!')
-
+button_coordinates: list = get_button_coordinates(width, height)
+button_center = get_center_of_the_button(button_coordinates)
+pyautogui.click(*button_center, interval=0.5, clicks=5)
 telegram.notify(message=f'Bro, i just click the button. Get ready to play!', token=os.environ['TELEGRAM_KEY'],
                 chat_id=os.environ['TELEGRAM_CHAT_ID'])
+
+# switch_from_cs_window()
